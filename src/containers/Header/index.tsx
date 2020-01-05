5fa831e47452db17614b74252f697e7c2c27c416
Ali Emir Sen <ali.sen@softtech.com.tr>
2020-01-05 20:54:36 +0300

    changes

--- src/containers/Header/index.tsx
+++ src/containers/Header/index.tsx
@@ -1,7 +1,12 @@
 import React, { FunctionComponent } from 'react'
+import cssStylez, { camelToKebab } from 'css-stylez'
+import styles from './Header.module.scss'
+import classNames from 'classnames'
+
+const stylez = cssStylez(styles, camelToKebab)
 
 const Header: FunctionComponent = () => {
-  return <h1>Sehir Bulmaca</h1>
+  return <h1 className={classNames(stylez.heading)}>Sehir Bulmaca</h1>
 }
 
 export default Header
